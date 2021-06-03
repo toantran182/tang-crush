074967cdafbecec7f8b80e78acd7cabca37afd2d
toantran182 <85275530+toantran182@users.noreply.github.com>
2021-06-03 13:40:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Em phải iu thưng anh nhó',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/toan.tran.182t
 }
