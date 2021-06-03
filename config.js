390350710fbf0760239abed643b18bc9a331cd7f
toantran182 <85275530+toantran182@users.noreply.github.com>
2021-06-03 19:15:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Em phải iu thưng anh nhó',
+    messDesc: 'Em phải iu thưng anh nhooó',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/toan.tran.182t
 }
